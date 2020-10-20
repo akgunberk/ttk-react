9bf7c7aa3fd6d3c9a938d132fcb4628dd7b79d98
Berk Akgün <berkakgun@armut.com>
2020-10-20 17:32:44 +0300

    realm-cli changes

---
+++ build/precache-manifest.dad3775662501d59aa74814e4efd73ab.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "332653575b4c981f3260fbb5b7a24774",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35d9c855203e77c54ad7",
+    "url": "/static/css/2.96745999.chunk.css"
+  },
+  {
+    "revision": "b592a545c59048839efc",
+    "url": "/static/css/main.114ff385.chunk.css"
+  },
+  {
+    "revision": "35d9c855203e77c54ad7",
+    "url": "/static/js/2.da82cff5.chunk.js"
+  },
+  {
+    "revision": "c3b8755184206352e6870827ce715a1e",
+    "url": "/static/js/2.da82cff5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b592a545c59048839efc",
+    "url": "/static/js/main.0f5063a3.chunk.js"
+  },
+  {
+    "revision": "9db8d5c44a4164dd852d",
+    "url": "/static/js/runtime-main.a57ed013.js"
+  },
+  {
+    "revision": "ee87944df7b534b8fbd2d4d0c4d30027",
+    "url": "/static/media/eg.ee87944d.png"
+  },
+  {
+    "revision": "eb7bf3daf641d11002b44e984610e5bb",
+    "url": "/static/media/pokemon-destek.eb7bf3da.png"
+  },
+  {
+    "revision": "7c6628a29a3fa0dff54199c29805f3df",
+    "url": "/static/media/ro.7c6628a2.png"
+  },
+  {
+    "revision": "bb92489430199ac5f9f52e642dc4c001",
+    "url": "/static/media/sa.bb924894.png"
+  },
+  {
+    "revision": "046e91f77483a277a434bac9ae4885e2",
+    "url": "/static/media/tr.046e91f7.png"
+  },
+  {
+    "revision": "b702632c379669a7aff671d001d082a2",
+    "url": "/static/media/uk.b702632c.png"
+  },
+  {
+    "revision": "2918610d97659f858bbbb7c226ed6842",
+    "url": "/static/media/us.2918610d.png"
+  }
+]);
